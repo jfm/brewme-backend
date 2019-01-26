507b994a7c21ef84b7f7246f87f010d249d2b8d9
Jesper Fussing Mørk <jfm@moerks.dk>
2019-01-26 20:43:43 +0100

    Added support for fetching recipes by name

--- brewme/api/api.py
+++ brewme/api/api.py
@@ -1,13 +1,16 @@
 import falcon
-from api.recipes.operations import Operations
+from brewme.api.recipes.operations import Recipes, RecipeByName
 from falcon_prometheus import PrometheusMiddleware
 
 def create():
     prometheus = PrometheusMiddleware()
-    recipes = Operations()
+    recipes = Recipes()
+    recipe_by_name = RecipeByName()
+
     api = falcon.API(middleware=prometheus) 
     api.add_route('/metrics', prometheus)
     api.add_route('/recipes', recipes)
+    api.add_route('/recipes/{name}', recipe_by_name)
     return api
 
 api = application = create()
--- brewme/api/recipes/operations.py
+++ brewme/api/recipes/operations.py
@@ -3,7 +3,7 @@ import jsonpickle
 from brewme.files.listing import Listing
 from brewme.beerxml.parser import Parser
 
-class Operations(object):
+class Recipes(object):
 
     """Docstring for Operations. """
 
@@ -13,9 +13,36 @@ class Operations(object):
 
     def on_get(self, request, response):
         recipes = []
-        for recipe_file in self.listing.list_files_in_folder('/home/jfm/Repositories/brewme/backend/tests/recipes'):
+        for recipe_file in self.listing.list_files_in_folder('/home/jfm/Repositories/brewme-backend/tests/recipes'):
             recipes_in_file = self.parser.parse_beerxml_file(recipe_file)
             recipes = recipes + recipes_in_file
 
+        response.set_header('Access-Control-Allow-Origin', '*')
         response.body = jsonpickle.encode(recipes, unpicklable=False)
         response.status = falcon.HTTP_200
+
+class RecipeByName(object):
+
+    """Docstring for RecipeByName. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        self.listing = Listing()
+        self.parser = Parser()
+
+    def on_get(self, request, response, name):
+        recipes = []
+        result = None
+        for recipe_file in self.listing.list_files_in_folder('/home/jfm/Repositories/brewme-backend/tests/recipes'):
+            recipes_in_file = self.parser.parse_beerxml_file(recipe_file)
+            recipes = recipes + recipes_in_file
+
+        for recipe in recipes:
+            if recipe.name == name:
+                result = recipe
+
+        response.set_header('Access-Control-Allow-Origin', '*')
+        response.body = jsonpickle.encode(result, unpicklable=False)
+        response.status = falcon.HTTP_200
+
+
